a9f2161de078a700d201b57ff416e9052f0e3f66
sebastopolET <82884592+sebastopolET@users.noreply.github.com>
2021-04-20 17:55:38 +0300

    Updating

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
-echo "Welcome to Sebas";
+$update = json_decode(file_get_contents("https://api.telegram.org/bot1746146428:AAF-ID1HMj-jpTCNyTqHE0w48RRjFKJRXP4/getUpdates"));
+
+echo "Welcome to Sebastopol :<br /> Command > ".$update['message']['text'];
 
 ?>
