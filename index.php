5761feb49a4f4543e523d2bc006a471924e3260f
sebastopolET <82884592+sebastopolET@users.noreply.github.com>
2021-04-21 23:28:47 +0300

    Up

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 $path = "https://api.telegram.org/bot1746146428:AAF-ID1HMj-jpTCNyTqHE0w48RRjFKJRXP4";
 
 $update = json_decode(file_get_contents("php://input"), TRUE);
@@ -7,6 +6,10 @@ $update = json_decode(file_get_contents("php://input"), TRUE);
 $chatId = $update["message"]["chat"]["id"];
 $message = $update["message"]["text"];
 
-file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=you said ".$message);
-
+if (strpos($message, "/weather") === 0) {
+$location = "Addis Ababa";
+$weather = "cold";
+   file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$location.": ". $weather);
+}
 ?>
+
