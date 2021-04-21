bc17a273fae78a6442bbd0b8c281720455f28937
sebastopolET <82884592+sebastopolET@users.noreply.github.com>
2021-04-21 23:25:22 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 
-$url = "https://api.telegram.org/bot1746146428:AAF-ID1HMj-jpTCNyTqHE0w48RRjFKJRXP4";
+$path = "https://api.telegram.org/bot1746146428:AAF-ID1HMj-jpTCNyTqHE0w48RRjFKJRXP4";
 
+$update = json_decode(file_get_contents("php://input"), TRUE);
 
+$chatId = $update["message"]["chat"]["id"];
+$message = $update["message"]["text"];
 
-file_get_contents($url."/sendMessage?chat_id=476779655&text=heroku_bot_says_hi");
-
-echo "Welcome to Sebastopol :<br /> Command > ";
+file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=you said ".$message);
 
 ?>
