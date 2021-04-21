23429a4217a913f0f983a903ba653c9d6713656e
sebastopolET <82884592+sebastopolET@users.noreply.github.com>
2021-04-22 00:00:50 +0300

    create mysql database

--- sebas.php
+++ sebas.php
@@ -1,3 +1,10 @@
 <?php
-   echo "My Sebastopol";
+ $conn = mysqli_connect( "http://sql3.freemysqlhosting.net", "sql3407329" , "h3tzsVCxA1" , "sql3407329") or die("couldn't connect to database"); ;
+
+ $query = "create table userdb(id int , name varchar(20))";
+ mysqli_query($query , $conn);
+
+ $query = "INSERT INTO userdb(id, name) VALUES(1 , 'testing')";
+ mysqli_query($query , $conn);
+
 ?>
