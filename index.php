0a089257490a7abf0ef855e6c1d0447afb4626fa
sebastopolET <82884592+sebastopolET@users.noreply.github.com>
2021-04-22 15:46:07 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,28 @@ $update = json_decode(file_get_contents("php://input"), TRUE);
 $chatId = $update["message"]["chat"]["id"];
 $message = $update["message"]["text"];
 file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=".$message);
+ $keyboard = [
+                            "keyboard" => [
+                                [
+                                    ["text" =>  urlencode('Get Started') ]
+                                    
+                                ]
+                            ],
+                        "resize_keyboard" => true
+                      ];
+            sendMessage( $chatId , $message , $keyboard );
+
+function sendMessage( $chatId , $text , $style){
+ 
+  if( strpos($style , "normal") === 0 ){
+           file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=".$text);
+   }
+  else {
+    file_get_contents($path. "/sendMessage?chat_id=". $chatId . "&text=" . urlencode("MENU") . "&reply_markup=" . json_encode($style));
+  }
+        
+}
+
 /*
 switch($message){
   case "/start" : 
