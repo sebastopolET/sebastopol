e1d951394d2272f49a2274cf21f8f10066be1cac
sebastopolET <82884592+sebastopolET@users.noreply.github.com>
2021-07-03 15:10:17 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-<?php
-
+//<?php
+/*
 $path = "https://api.telegram.org/bot1746146428:AAF-ID1HMj-jpTCNyTqHE0w48RRjFKJRXP4";
 
 $update = json_decode(file_get_contents("php://input"), TRUE);
@@ -71,6 +71,26 @@ else if( substr( $message , 0 , 3)  == "www"){
 else{
         file_get_contents($path."/sendMessage?chat_id=".$chatId."&text=".$message);
 }
+
+*/
+
+
+<?php
+   $path = "https://api.telegram.org/bot1703520207:AAHWzisIZxsltY6a0YQhMpUcAA8zrZxyTK4";
+   
+
+ if(isset($_GET['from']) && isset($_GET['text'])){
+       $from = $_GET['from'];
+       $text = $_GET['text'];
+       echo "From ".$from." Message Body ".$text;
+       file_get_contents($path."/sendMessage?chat_id=1788583880&text=From ".$from."Message Body ".$text);
+   }
+
+ else{
+    echo "Message Failed";
+    file_get_contents($path."/sendMessage?chat_id=1788583880&text='message failed'");
+    }
+//?>
 
 
 ?>
